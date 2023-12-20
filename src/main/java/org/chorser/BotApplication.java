622b43828e62f2da682cd957141ef6216dfd420d
Cursed_Owl <cymogeko@163.com>
2023-12-20 19:31:41 +0800

    Change basic framework to Javacord

--- src/main/java/org/chorser/BotApplication.java
+++ src/main/java/org/chorser/BotApplication.java
@@ -1,36 +1,37 @@
 package org.chorser;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
+
 import org.chorser.entity.Authentication;
-import org.chorser.listener.DefaultListener;
 import org.chorser.util.YmlReader;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.misc.BASE64Decoder;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.Base64;
 
 public class BotApplication {
 
     private static final String CONFIG_PATH="config.yml";
-
-    private static final BASE64Decoder base64Decoder=new BASE64Decoder();
+    private static final Base64.Decoder base64Decoder=Base64.getDecoder();
 
     private static Logger log= LoggerFactory.getLogger(BotApplication.class);
 
     public static void main(String[] args) {
+
         try {
             Authentication authentication = YmlReader.readAuthentication(CONFIG_PATH);
             if(authentication==null){
                 throw new IOException();
             }
             if(authentication.getToken()!=null){
-                byte[] bytes = base64Decoder.decodeBuffer(authentication.getToken());
+                byte[] bytes = base64Decoder.decode(authentication.getToken());
                 String token = new String(bytes);
                 log.info(token);
-                initialDiscordConnection(token);
+                initialJavacordConnection(token);
             }
 
         } catch (Exception e) {
@@ -40,9 +41,22 @@ public class BotApplication {
 
     }
 
-    private static void initialDiscordConnection(String token) {
-        JDA build = JDABuilder.createDefault(token)
-                .addEventListeners(new DefaultListener())
-                .build();
+    private static void initialJavacordConnection(String token) {
+
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 10809));
+        DiscordApi api = new DiscordApiBuilder()
+                .setProxy(proxy)
+                .setToken(token)
+                .setAllIntents()
+                .addMessageCreateListener(event -> {
+                    if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                        event.getChannel().sendMessage("Pong!");
+                    }
+                })
+                .login()
+                .join();
+
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
     }
+
 }
--- src/main/java/org/chorser/listener/DefaultListener.java
+++ src/main/java/org/chorser/listener/DefaultListener.java
@@ -1,15 +1,6 @@
 package org.chorser.listener;
 
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class DefaultListener extends ListenerAdapter {
+public class DefaultListener{
 
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        Message message = event.getMessage();
-        String contentRaw = message.getContentRaw();
-        System.out.println(contentRaw);
-    }
 }
